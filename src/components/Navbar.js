7d33ee118fa40284bbc24317677033a4de3946ff
Hamza ince <hamzaince10@gmail.com>
2024-01-18 12:16:09 +0300

    project continue

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,48 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { MdTravelExplore } from "react-icons/md";
+import { click } from '@testing-library/user-event/dist/click';
 
 
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+
   return (
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to="/" className='navbar-logo'>
-            TRAVEL <i className='fab fa-typo3'></i>
+            TRAVEL <i class="fa-regular fa-compass"></i>
           </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times' : 'fas fa-bars'}></i>
+          </div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className='nav-item'>
+              <Link to={'/'} className='nav-links' onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+             <li className='nav-item'>
+              <Link to={'/services'} className='nav-links' onClick={closeMobileMenu}>
+                Services
+              </Link>
+            </li>
+             <li className='nav-item'>
+              <Link to={'/products'} className='nav-links' onClick={closeMobileMenu}>
+                Products
+              </Link>
+            </li>
+             <li className='nav-item'>
+              <Link to={'/sign-up'} className='nav-links-mobile' onClick={closeMobileMenu}>
+                sign-up
+              </Link>
+            </li>
+          </ul>
         </div>
       </nav>
     </>
