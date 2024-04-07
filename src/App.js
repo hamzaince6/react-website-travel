aadb40ef868030322ac9c9b5ffec322c45648fb5
Muhammet Hamza İnce <hamzaince10@gmail.com>
2024-04-07 04:20:18 +0300

    react course continue

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import Navbar from "./components/Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from "./components/Navbar";
 import Home from './components/pages/Home';
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUP from './components/pages/SignUP';
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/sign-up" element={<SignUP />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
